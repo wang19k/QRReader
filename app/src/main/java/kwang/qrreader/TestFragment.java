cd97a23604139a0692e8040301f050fa6d9bcadc
Oliver Ren <oliveren@mit.edu>
2016-01-25 20:15:01 -0500

    added a back button

--- app/src/main/java/kwang/qrreader/TestFragment.java
+++ app/src/main/java/kwang/qrreader/TestFragment.java
@@ -1,6 +1,8 @@
 package kwang.qrreader;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +12,22 @@ import android.widget.Button;
 /**
  * Created by Oliver on 1/25/2016.
  */
-public class TestFragment extends Fragment{
+public class TestFragment extends Fragment implements Button.OnClickListener {
 
     private Button exit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.test_fragment, container, false);
+        View view = inflater.inflate(R.layout.test_fragment, container, false);
+        exit = (Button) view.findViewById(R.id.button);
+        exit.setOnClickListener(this);
+        return view;
     }
+
+    public void onClick(View v) {
+        getActivity().getFragmentManager().beginTransaction().remove(this).commit();
+    }
+
 }
